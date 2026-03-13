4be7416923a27531b5c53ab61b2f8c65dfbadb78
sergiomartinezhernandez <sergio.martinezhernandez@ticketmaster.com>
2026-03-12 21:15:13 -0600

    connected to database

---
+++ src/main/java/com/gobierno/bienestarbank/BienestarbankApplication.java
@@ -0,0 +1,13 @@
+package com.gobierno.bienestarbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienestarbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienestarbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gobierno/bienestarbank/BienestarbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobierno.bienestarbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienestarbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
